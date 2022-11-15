c19044200ec1a0202dbfb213759a4706979f7ae4
Antoine MAILLARD <ash.uncover@gmail.com>
2022-11-15 18:15:05 +0100

    improved icontabbar

---
+++ src/lib/constants/IconTabBarDesign.ts
@@ -0,0 +1,14 @@
+export type IconTabBarDesign =
+  'icon' |
+  'icon-only' |
+  'counters'
+
+export const IconTabBarDesigns: {
+  ICON: IconTabBarDesign,
+  ICON_ONLY: IconTabBarDesign,
+  COUNTERS: IconTabBarDesign,
+} = {
+  ICON: 'icon',
+  ICON_ONLY: 'icon-only',
+  COUNTERS: 'counters',
+}
